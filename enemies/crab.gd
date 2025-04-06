eb64431d420372cb970a2744b8392ea58c44792f
Zshandi Krahn <ZshandiK@gmail.com>
2025-04-06 16:46:10 -0700

    Add crab movement
    mr crab bounces off the walls

--- enemies/crab.gd
+++ enemies/crab.gd
@@ -1,22 +1,25 @@
+@tool
 extends Enemy
 
 @export
-var movement_speed := 5.0
+var movement_speed := 20.0
 
 @export_enum("Left:-1", "Right:1")
-var initial_direction := -1
+var initial_direction:int
 
 var starting_position:Vector2
 var current_direction:int
 
 func _ready() -> void:
 	super._ready()
+	if initial_direction == 0:
+		initial_direction = -1
 	current_direction = initial_direction
 
 func _physics_process(delta: float) -> void:
 	if not activated: return
 	velocity = Vector2(current_direction * movement_speed, 0) + get_gravity()
-	print_debug("velocity: ", velocity)
+	print_debug("current_direction: ", current_direction)
 	move_and_slide()
 	
 	if is_on_wall():
