0ac7118de0c8aab674d806de3697d52c7b74e7f9
Zshandi Krahn <ZshandiK@gmail.com>
2025-04-06 14:04:28 -0700

    Add attack

--- character.gd
+++ character.gd
@@ -4,9 +4,9 @@ class_name Character
 # Constants
 
 var max_speed_x := 35.0
-var accel_x := 0.5
-var decel_x := 1.0
-var break_x := 2.0
+var accel_x := 30.0
+var decel_x := 60.0
+var break_x := 120.0
 
 var last_direction := 1.0
 var current_direction := 0.0
@@ -14,8 +14,8 @@ var current_direction := 0.0
 var max_speed_y := 55.0
 var max_speed_float := 30.0
 var jump_speed := 55.0
-var float_accel := 0.7
-var jump_accel := 0.8
+var float_accel := 42.0
+var jump_accel := 48.0
 
 var double_tap_interval_ms := 400
 
@@ -30,6 +30,10 @@ var is_floating := false
 
 var invincible_frames := false
 
+var is_attacking := false
+var is_attack_windup := false
+var should_release_attack := false
+
 # Stats
 
 var health := 5
@@ -38,7 +42,13 @@ var health := 5
 
 func _process(delta: float) -> void:
 	%AnimatedSprite.scale.x = last_direction
-	if is_on_floor():
+	if is_attacking:
+		if is_attack_windup:
+			if %AnimatedSprite.animation != &"attack_windup":
+				%AnimatedSprite.play("attack_windup")
+		else:
+			%AnimatedSprite.play("attack_swing")
+	elif is_on_floor():
 		if current_direction != 0:
 			%AnimatedSprite.play("walking")
 		else:
@@ -46,7 +56,41 @@ func _process(delta: float) -> void:
 	else:
 		%AnimatedSprite.play("jump")
 
-func _physics_process(delta: float) -> void:
+func process_attack(delta: float) -> void:
+	if is_attacking:
+		if not Input.is_action_pressed("action"):
+			should_release_attack = true
+		if not %AnimatedSprite.is_playing():
+			if is_attack_windup:
+				if should_release_attack:
+					should_release_attack = false
+					is_attack_windup = false
+					for body in %HammerHit_0.get_overlapping_bodies():
+						if "player_attack" in body:
+							body.player_attack()
+					await get_tree().create_timer(0.35).timeout
+					var enemy_found = null
+					for body in %HammerHit_1.get_overlapping_bodies():
+						if "player_attack" in body:
+							body.player_attack()
+							if "global_position" in body:
+								enemy_found = body.global_position
+					if enemy_found != null:
+						knockback(enemy_found, 5)
+					else:
+						await get_tree().create_timer(0.05).timeout
+						%HammerHitAudio.play()
+			else:
+				is_attacking = false
+	else:
+		if is_on_floor() and Input.is_action_just_pressed("action"):
+			velocity = Vector2.ZERO
+			is_attacking = true
+			is_attack_windup = true
+			should_release_attack = false
+	
+
+func process_movement(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("jump"):
 		var current_time := Time.get_ticks_msec()
@@ -57,7 +101,7 @@ func _physics_process(delta: float) -> void:
 			jump_pressed_time_ms = current_time
 	
 	if is_floating:
-		velocity.y -= float_accel
+		velocity.y -= float_accel * delta
 		if is_on_ceiling():
 			is_floating = false
 	else:
@@ -69,7 +113,7 @@ func _physics_process(delta: float) -> void:
 		is_jumping = true
 	elif is_jumping and velocity.y < 0:
 		if Input.is_action_pressed("jump"):
-			velocity.y -= jump_accel
+			velocity.y -= jump_accel * delta
 		else:
 			is_jumping = false
 	
@@ -91,13 +135,16 @@ func _physics_process(delta: float) -> void:
 	
 	if !is_on_floor():
 		x_change *= 0.2
-	velocity.x += x_change
+	velocity.x += x_change * delta
 	
 	if is_floating:
 		velocity.y = clampf(velocity.y, -max_speed_float, max_speed_float)
 	else:
 		velocity.y = clampf(velocity.y, -max_speed_y, max_speed_y)
 	velocity.x = clampf(velocity.x, -max_speed_x, max_speed_x)
+	
+
+func process_collision(delta: float) -> void:
 	
 	move_and_slide()
 	
@@ -109,17 +156,26 @@ func _physics_process(delta: float) -> void:
 		if what is Node and "hurt_player" in what:
 			what.hurt_player(self)
 
+func _physics_process(delta: float) -> void:
+	process_attack(delta)
+	if not is_attacking:
+		process_movement(delta)
+	process_collision(delta)
+
 func take_damage(amount:int, from:Node2D = null, knockback:float = 40):
 	if invincible_frames: return
 	
 	health -= amount
-	$AnimationPlayer.play("hurt_flash")
+	%AnimationPlayer.play("hurt_flash")
 	invincible_frames = true
 	if from != null and knockback > 0:
-		var direction_x := signf(global_position.x - from.global_position.x)
-		var direction := Vector2(direction_x, -0.7).normalized()
-		velocity = knockback * direction
+		knockback(from.global_position, knockback)
 	
 	await get_tree().create_timer(1.2).timeout
-	$AnimationPlayer.play("RESET")
+	%AnimationPlayer.play("RESET")
 	invincible_frames = false
+
+func knockback(from_global:Vector2, amount:float):
+	var direction_x := signf(global_position.x - from_global.x)
+	var direction := Vector2(direction_x, -0.7).normalized()
+	velocity = amount * direction
--- crab.gd
+++ crab.gd
@@ -3,5 +3,12 @@ extends CharacterBody2D
 func _ready() -> void:
 	%Animation.play("default")
 
-func hurt_player(player:Character):
+func hurt_player(player:Character) -> void:
 	player.take_damage(1, self)
+
+func player_attack() -> void:
+	%DeathSound.play()
+	hide()
+	process_mode = Node.PROCESS_MODE_DISABLED
+	await %DeathSound.finished
+	queue_free()
