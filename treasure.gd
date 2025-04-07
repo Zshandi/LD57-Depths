cde4fca7575f46a092bbbe3acd13a4ce3993388f
Zshandi Krahn <ZshandiK@gmail.com>
2025-04-06 23:43:21 -0700

    Add collected signal to treasure

--- treasure.gd
+++ treasure.gd
@@ -1,10 +1,13 @@
 extends Enemy
 
+signal collected
+
 func player_attack(character:Character):
 	var bubbles = preload("res://bubbles.tscn").instantiate()
 	bubbles.global_position = global_position
 	add_sibling(bubbles)
 	character.clear_panic_mode()
+	collected.emit()
 	super.player_attack(character)
 
 func hurt_player(player):
